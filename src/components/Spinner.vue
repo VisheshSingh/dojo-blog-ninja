1db7a09faf33ba6f678da7045eab7180707f7eae
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-27 19:41:21 -0600

    Spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="spinner"></div>
+</template>
+
+<script>
+export default {
+  name: 'Spinner',
+};
+</script>
+
+<style>
+.spinner {
+  width: 40px;
+  height: 40px;
+  margin: 30px auto;
+  border-radius: 50%;
+  border: 3px solid transparent;
+  border-top-color: #ff8800;
+  animation: spin 1s ease infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotateZ(360deg);
+  }
+}
+</style>
